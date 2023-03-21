f2c793323298fabe7de8d290b604934100fc59a3
Sidahmed Belkadi <sidahmed.belkadi0@gmail.com>
2023-03-21 12:31:24 +0100

    Final Commit

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TasksController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Public Routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Protected Routes
+Route::middleware(['auth:sanctum'])
+    -> group(function (){
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::apiResource('/tasks', TasksController::class);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
